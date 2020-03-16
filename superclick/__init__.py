5d055c4f2d16085ad1d7a5e948a416f3c04f1e4b
Python Whiz 256 <mostwonderfulboy@gmail.com>
2020-03-15 21:04:51 -0600

    Right click and speed
    - Added support for right-click
    - Sped things up

--- superclick/__init__.py
+++ superclick/__init__.py
@@ -5,22 +5,27 @@ Key = pynput.keyboard.Key
 CharToCode = pynput.keyboard.KeyCode.from_char
 Button = pynput.mouse.Button
 
-HOT_KEY =   CharToCode("s")
-CLICK_KEY = CharToCode("c")
-STOP_KEY =  CharToCode("q")
+HOT_KEY =           CharToCode("s")
+LEFT_CLICK_KEY =    CharToCode("c")
+RIGHT_CLICK_KEY =   CharToCode("v")
+STOP_KEY =          CharToCode("q")
 
 is_hot_pressed =    False
-is_combo_active =   False
+is_left_active =    False
+is_right_active =   False
 is_stopped =        False
 
 def on_press(key):
     global is_hot_pressed
-    global is_combo_active
+    global is_left_active
+    global is_right_active
     global is_stopped
     if key == HOT_KEY:
         is_hot_pressed = True
-    elif key == CLICK_KEY and is_hot_pressed:
-        is_combo_active = True
+    elif key == LEFT_CLICK_KEY and is_hot_pressed:
+        is_left_active = True
+    elif key == RIGHT_CLICK_KEY and is_hot_pressed:
+        is_right_active = True
     elif key == STOP_KEY and is_hot_pressed:
         is_stopped = True
         return False
@@ -28,19 +33,25 @@ def on_press(key):
 
 def on_release(key):
     global is_hot_pressed
-    global is_combo_active
+    global is_left_active
+    global is_right_active
     if key == HOT_KEY:
         is_hot_pressed = False
-        is_combo_active = False
-    elif key == CLICK_KEY:
-        is_combo_active = False
+        is_left_active = False
+        is_right_active = False
+    elif key == LEFT_CLICK_KEY:
+        is_left_active = False
+    elif key == RIGHT_CLICK_KEY:
+        is_right_active = False
     
 kl = pynput.keyboard.Listener(on_press=on_press, on_release=on_release)
 kl.start()
 
 while not is_stopped:
-    if is_combo_active:
+    if is_left_active:
         mc.press(Button.left)
         mc.release(Button.left)
-        time.sleep(0.01)
-        print("COMBO ACTIVE!")
+    if is_right_active:
+        mc.press(Button.right)
+        mc.release(Button.right)
+    time.sleep(0.0075)
